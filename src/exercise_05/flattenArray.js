62183ae5ea0d64d02d5fd8853d372fb0cf05ad1f
Zhang Jin <zj574406254@hotmail.com>
2020-09-03 17:38:21 +0800

    [ZhangJin] feat:finish exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 export default function flattenArray(array) {
   // This function flattens a nested array into a sequence.
   //
@@ -6,5 +7,18 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error("Flatten undefined or null array");
+  }
+  let re=[];
+  for (let i=0; i<array.length; i++) {
+    if (array[i].length!=undefined) {
+      for(let j=0;j<array[i].length;j++){
+        re.push(array[i][j]);
+      }
+    } else {
+      re.push(array[i]);
+    }
+  }
+  return re;
 }
