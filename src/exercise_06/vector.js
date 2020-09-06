f643b11c74c1e2039be80ecbffe6b4f585dd1689
Zhang Jin <zj574406254@hotmail.com>
2020-09-06 11:22:46 +0800

    [ZhangJin] feat:finish exercise_6

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-properties */
+/* eslint-disable class-methods-use-this */
 export default class Vector {
   // This class represents a 2-dimensional vector. Please implement the class according to the
   // following instructions:
@@ -13,4 +15,37 @@ export default class Vector {
   // class.
 
   // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  distance() {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(xx) {
+    throw new Error('should not set x and y of the vector');
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  set y(_yy) {
+    throw new Error('should not set x and y of the vector');
+  }
+
+  static plus(vec1, vec2) {
+    return new Vector(vec1.x + vec2.x, vec1.y + vec2.y);
+  }
+
+  static minus(vec1, vec2) {
+    return new Vector(vec1.x - vec2.x, vec1.y - vec2.y);
+  }
 }
