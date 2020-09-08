014e4d748d2d8b07bbb4708ec8c459212de06400
Zhang Jin <zj574406254@hotmail.com>
2020-09-08 17:58:37 +0800

    [ZhangJin] feat:finish exercise_07

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 export default function waitForAll(...promises) {
   // This function returns a promise which will be triggered when all the `promises` are completed.
   //
@@ -9,5 +10,47 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+
+  promises.forEach((pro) => {
+    if (!(pro instanceof Promise)) {
+      throw new Error('Not all elements are promises.');
+    }
+  });
+  // return Promise.allSettled(promises);
+  // const promises = Array.from(iterators);
+  const num = promises.length;
+  const settledList = new Array(num);
+  let settledNum = 0;
+  let isSuccess = true;
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      Promise.resolve(promise)
+        .then(() => {
+          if (++settledNum === num) {
+            if (isSuccess) {
+              resolve(settledList);
+            } else {
+              reject(settledList);
+            }
+          }
+        }, () => {
+          isSuccess = false;
+          if (++settledNum === num) {
+            reject(settledList);
+          }
+        });
+    });
+  });
+  // p.then(() => {
+  //   if (isSuccess) {
+  //     return new Promise((resolve) => {
+  //       resolve();
+  //       // reject();
+  //     });
+  //   }
+  //   return new Promise((resolve, reject) => {
+  //     // resolve();
+  //     reject();
+  //   });
+  // });
 }
